deb39c0e0703ee0c5307518a9ca23a8bab9085d3
PolyannaMeira <polyannaxam@gmail.com>
2025-10-25 17:30:13 +0200

    test: add ping function for Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,6 @@
-
+// api/ping.ts
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../../src/app"; 
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // Garante que o Fastify e as rotas estão prontos
-  await app.ready();
-
-  // Encaminha a requisição da Vercel para o servidor HTTP interno do Fastify
-  app.server.emit("request", req, res);
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({ ok: true, time: new Date().toISOString() });
 }
