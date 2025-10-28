5faa75f6a5f2c3861da7f5519a46f0be37ba4fe5
PolyannaMeira <polyannaxam@gmail.com>
2025-10-28 14:22:05 +0100

    ajuste vercel

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
 import Fastify from "fastify";
-import cors from "@fastify/cors";
+import fastifyCors from '@fastify/cors';
+
 import routes from "./routes";
 
 const app = Fastify({ logger: true });
 
+//const allowedOrigin = 'https://dev-bills-front-8skduh2ay-polyannas-projects.vercel.app';
+
 // CORS ABERTO (teste)
-app.register(cors, {
-  origin: true, // reflete qualquer Origin
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: false, // com Bearer, deixe false
-});
+app.register(fastifyCors, { origin: true });
+
+/*app.register(fastifyCors, {
+  origin: (origin, cb) => {
+    // Se origin for undefined (requests via curl/postman) permitir:
+    if (!origin) return cb(null, true);
+    // permitir apenas a origem do front ou use allowedOrigins.includes(origin)
+    if (origin === allowedOrigin) return cb(null, true);
+    // negar outras origens:
+    cb(new Error('Not allowed'), false);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  // preflight continuará sendo tratado automaticamente
+});*/
 
 // prefixo: na Vercel, use "/" (o /api externo já é dela)
 const isVercel = !!process.env.VERCEL;
